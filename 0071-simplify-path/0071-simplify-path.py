524cc9604b69f0d43654c69b0fae87f041632b2e
kurantoB <59395001+kurantoB@users.noreply.github.com>
2023-12-14 23:25:56 -0500

    Time: 19 ms (50.67%), Space: 13.5 MB (42.66%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.py
+++ 0071-simplify-path/0071-simplify-path.py
@@ -4,7 +4,6 @@ class Solution(object):
         :type path: str
         :rtype: str
         """
-        #stack = Stack()
         stack = deque()
         
         elems = path.split('/')
@@ -17,43 +16,15 @@ class Solution(object):
                 if len(stack) is not 0:
                     stack.pop()
                 continue
-            #newElem = StackElem(elem)
-            #stack.push(newElem)
             stack.append(elem)
         
         canonicalPath = ''
         
-        #while not stack.isEmpty():
         while len(stack) is not 0:
-            #elem = stack.pop()
             elem = stack.pop()
-            #canonicalPath = '/' + elem.data + canonicalPath
             canonicalPath = '/' + elem + canonicalPath
 
         if len(canonicalPath) == 0:
             canonicalPath = '/'
         
         return canonicalPath
-
-class StackElem:
-    next = None
-    data = None
-    
-    def __init__(self, data):
-        self.data = data
-        
-class Stack:
-    head = None
-    
-    def push(self, elem):
-        if self.head is not None:
-            elem.next = self.head
-        self.head = elem
-    
-    def pop(self):
-        tmp = self.head
-        self.head = self.head.next
-        return tmp
-    
-    def isEmpty(self):
-        return self.head == None
